6da3110df49c21931f397361c23e9fd4b3110eac
Rupert Swarbrick <rupert.swarbrick@argondesign.com>
2018-06-29 10:21:59 +0100

    Add trivial option parser
    This gains a dependency on optparse-applicative

--- Main.hs
+++ Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Args = Args
+  { input :: FilePath
+  }
+
+mainArgs :: Parser Args
+mainArgs = Args
+           <$> argument str ( metavar "input" <>
+                              help "Input file" )
+
+mainInfo :: ParserInfo Args
+mainInfo = info (mainArgs <**> helper)
+           ( fullDesc <>
+             progDesc "Generate functional coverage bindings" <>
+             header "acov - functional coverage bindings generator" )
+
+run :: Args -> IO ()
+run args = putStrLn (input args)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = execParser mainInfo >>= run
