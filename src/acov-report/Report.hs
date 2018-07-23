834681b12011a23a65c598fd34e7a1209a8bace1
Rupert Swarbrick <rupert.swarbrick@argondesign.com>
2018-07-23 11:38:50 +0100

    Just report first few misses
    I think this now acheives the absolute bare minimum for a useful
    coverage report.

--- src/acov-report/Report.hs
+++ src/acov-report/Report.hs
@@ -3,10 +3,16 @@ module Report
   ) where
 
 import Control.Exception.Base
+import Control.Monad
 import Data.Bits
+import Data.List
 import qualified Data.Set as Set
 import System.IO
 
+import Ranged
+import SymbolTable
+
+import Parser (symName)
 import qualified Width as W
 import Merge
 
@@ -18,7 +24,7 @@ report h (Coverage mods) =
 
 reportMod :: Handle -> ModCoverage -> IO ()
 reportMod h (ModCoverage name scopes) =
-  hPutStr h ("<h1>" ++ name ++ "</h1>") >>
+  hPutStr h ("<h1>Module " ++ name ++ "</h1>") >>
   mapM_ (reportScope h) scopes
 
 reportScope :: Handle -> ScopeCoverage -> IO ()
@@ -28,33 +34,63 @@ reportScope h (ScopeCoverage name grps) =
   reportGrp h (head grps) >>
   mapM_ (\ g -> hPutStr h "<hr/>" >> reportGrp h g) grps
 
-cross :: [W.Record] -> [([Integer], Integer, Int)]
-cross recs =
+cross' :: [W.Record] -> [([Integer], Integer, Int)]
+cross' recs =
   assert (not $ null recs) $
   if length recs == 1 then map (\ n -> ([n], n, w)) clist
-  else concatMap f $ cross (tail recs)
+  else concatMap f $ cross' (tail recs)
   where
     r0 = head recs
     w = W.recWidth r0
     clist = W.recClist r0
     f (rst, n', w') = map (\ n -> (n : rst, shiftL n w' + n', w + w')) clist
 
-showLine :: GroupCoverage -> ([Integer], Integer, Int) ->
-             (Int, Int, [String]) -> (Int, Int, [String])
-showLine gc (vars, val, w) (hits, count, strs) =
-  (if isHit then 1 + hits else hits, 1 + count,
-   ("<span class='cov-" ++ hitmiss ++ "'>" ++ tickcross ++ "</span>") : strs)
-  where isHit = Set.member val (gcVals gc)
-        hitmiss = if isHit then "1" else "0"
-        tickcross = if isHit then tick else cross
-        tick = "&#x2713;"
-        cross = "&#x274c;"
+type Entry = ([Integer], Integer)
+
+cross :: [W.Record] -> [Entry]
+cross = map (\ (vals, val, _) -> (vals, val)) . cross'
+
+{-
+  This runs through the crossed values, counting up how many of them
+  we've managed to hit. We also collect the first 10 values that we
+  missed.
+-}
+countHits :: GroupCoverage -> (Int, Int, [Entry])
+countHits gc =
+  let (hits, cnt, badleft, bads) =
+        foldl' f (0, 0, 10, []) (cross (gcRecs gc)) in
+    (hits, cnt, reverse bads)
+  where f (hits, cnt, badleft, bads) (vals, val) =
+          if Set.member val (gcVals gc) then
+            (1 + hits, 1 + cnt, badleft, bads)
+          else if badleft > 0 then
+            (hits, 1 + cnt, badleft - 1, (vals, val) : bads)
+          else
+            (hits, 1 + cnt, badleft, bads)
+
+showMiss :: Handle -> Entry -> IO ()
+showMiss h (vals, val) =
+  hPutStr h ("<tr><td>" ++ show val ++ "</td></tr>")
+
+grpName :: GroupCoverage -> String
+grpName gc =
+  assert (not $ null recs) $
+  if length recs == 1 then recName (head recs)
+  else intercalate ", " (map recName recs)
+  where recs = gcRecs gc
+        recName r = symName $ rangedData $
+                    stNameAt (rangedData (W.recSym r)) (gcST gc)
 
 reportGrp :: Handle -> GroupCoverage -> IO ()
 reportGrp h gc =
-  put ("<p>Group (" ++ show hits ++ "/" ++ show count ++ ")</p><table>") >>
-  mapM_ reportLine items >>
-  put "</table>"
+  put ("<p>" ++ name ++ " (" ++ show hits ++ "/" ++ show count ++ ")</p>") >>
+  if hits /= count then
+    assert (not $ null $ firstMisses) $
+    put ("<p>First " ++ show (length firstMisses) ++ " misses:</p><table>") >>
+    mapM_ (showMiss h) firstMisses >>
+    put "</table>"
+  else
+    return ()
   where put = hPutStr h
-        (hits, count, items) = foldr (showLine gc) (0, 0, []) (cross (gcRecs gc))
-        reportLine str = put ("<tr><td>" ++ str ++ "</td></tr>")
+        name = grpName gc
+        (hits, count, firstMisses) = countHits gc
