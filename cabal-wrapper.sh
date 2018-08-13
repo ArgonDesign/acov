1611eb619e9b89c54d8f4b5c61c2d93e26ab693a
Rupert Swarbrick <rupert.swarbrick@argondesign.com>
2018-08-13 17:34:15 +0100

    Try again for Cabal magic -j parameters
    The new "-j" parameter appeared with version 1.20 (not 2.0).

--- cabal-wrapper.sh
+++ cabal-wrapper.sh
@@ -11,19 +11,24 @@ fi
 SUBCMD="$1"
 shift
 
-# The build subcommand in Cabal version 2.x supports a -j parameter
-# for the number of cores to use. The default is to use the number of
-# cores on the machine. There's also a sanity check where if you say
-# "-j100" it moans at you, pointing out that would be a crazy thing to
-# do. Unfortunately, running without -j on an enormous machine hits
-# this error. Aargh!
+# The build subcommand in Cabal from version 1.20.0.0 supports a -j
+# parameter for the number of cores to use. The default is to use the
+# number of cores on the machine. There's also a sanity check where if
+# you say "-j100" it moans at you, pointing out that would be a crazy
+# thing to do. Unfortunately, running without -j on an enormous
+# machine hits this error. Aargh!
+#
+# We decide that Cabal is too old to need "-j" if its version string
+# starts with "1.1".
 CABAL_DASH_J=""
 if [ x"$SUBCMD" = xbuild ]; then
-    CABAL_MAJOR="$(cabal --version | grep 'of the Cabal library' | \
-                    sed 's/compiled using version \([^ .]*\).*/\1/')"
-    if [ x"$CABAL_MAJOR" = x2 ]; then
-        if [ ! "$(nproc)" -le 40 ]; then
-            CABAL_DASH_J="-j40"
+    if cabal --version | \
+            grep 'version 1\.1.*of the Cabal library' >/dev/null; then
+        # Old Cabal. No -j needed.
+        CABAL_DASH_J=""
+    else
+        if [ ! "$(nproc)" -le 4 ]; then
+            CABAL_DASH_J="-j4"
         fi
     fi
 fi
